0f9b223ce978a4609f5d3ede0548f5c3f27df4d8
Florencia Nogueira <florencia.nogueira@estudiantes.utec.edu.uy>
2024-11-02 17:28:45 -0300

    Create persona.h

---
+++ persona.h
@@ -0,0 +1,23 @@
+#ifndef PERSONA_H
+#define PERSONA_H
+#include <iostream>
+#include <cstring>
+
+const int MAX_PERSONAS = 100;
+
+struct _persona {
+    char nombre[50];
+    char ci[8];
+    int numSubordinados;
+    _persona* subordinados[MAX_PERSONAS];
+};
+typedef _persona* Persona;
+
+// Funciones específicas de Persona
+void inicializarPersona(Persona persona, const char* nombre, const char* ci);
+Persona buscarCargo(Persona node, const char* cargo);
+void listarPersonasRec(Persona node, const char* cargo);
+void eliminarOrganigramaRec(Persona node);
+bool eliminarPersonaRec(Persona node, const char* ci);
+
+#endif
